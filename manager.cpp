8e2f53e3ac0d337eafde4b854f0ef49288ccab10
SygyzyH <76969579+SygyzyH@users.noreply.github.com>
2021-10-13 18:14:23 +0300

    Added Spheres to scene
    Added spheres to default scene

--- manager.cpp
+++ manager.cpp
@@ -22,12 +22,24 @@ std::mutex mutex;
 std::thread RTThread(threadDriver);
 
 void ManagerInit() {
-    std::vector<poly *> mesh = parseObjectFiles(1, "objects/test.obj");
+    std::vector<poly *> mesh = parseObjectFiles(1, "objects/axis.obj");
     poly **nativeMesh = (poly **) malloc(mesh.size() * sizeof(poly *));
     for (int i = 0; i < mesh.size(); i++)
         nativeMesh[i] = mesh[i];
 
-    RTInit(NULL, 0, nativeMesh, mesh.size());
+    sphere **nativeContext = (sphere **) malloc(sizeof(sphere *));
+    nativeContext[0] = (sphere *) malloc(sizeof(sphere));
+    vec *pos = (vec *) malloc(sizeof(vec));
+    pos->size = 3;
+    pos->value = (float *) malloc(3 * sizeof(float));
+    pos->value[0] = 0.5f;
+    pos->value[1] = -1;
+    pos->value[2] = 0.5f;
+    nativeContext[0]->pos = pos;
+    nativeContext[0]->color = 0x007f7f00;
+    nativeContext[0]->radius = 0.2;
+    nativeContext[0]->reflectivity = 1;
+    RTInit(nativeContext, 1, nativeMesh, mesh.size());
     RTThread.detach();
     buffer1 = RTEntryPoint();
     buffer2 = RTEntryPoint();
